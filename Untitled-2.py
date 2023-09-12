8d5d9efcdc5f9aeb6f97a53a6feb0946f20449ee
Kartike <139012197+Kartike007@users.noreply.github.com>
2023-09-13 00:50:45 +0530

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,25 @@
+from playsound import playsound
+print("No: of songs available\n1.we dont talk\n2.-Dangerously\n3.Hum Tere Bin Ab Reh Nahi Sakte\n4.Stay\n5.OneKiss+ I Was Never there\n6.This Is What You Came For\n7.Senorita\n8.Bitches Come and go\n9.All My Friends are Toxic")
+order=input("Enter the song you want to listen=")
+if "we dont talk" in order:
+    playsound("C:\\Users\\Dell\\Downloads\\we dont talk.mp3")
+elif "-Dangerously" in order:
+    playsound("C:\\Users\\Dell\\Downloads\\-Dangerously.mp3")
+elif "Hum Tere Bin Ab Reh Nahi Sakte" in order:
+    playsound("C:\\Users\\Dell\\Downloads\\Hum Tere Bin Ab Reh Nahi Sakte.mp3")
+elif "Stay" in order:
+    playsound("C:\\Users\\Dell\\Downloads\\Stay.mp3")
+elif "OneKiss+ I Was Never there" in order:
+    playsound("C:\\Users\\Dell\\Downloads\\OneKiss+ I Was Never there.mp3")
+elif "This Is What You Came For" in order:
+    playsound("C:\\Users\\Dell\\Downloads\\This Is What You Came For.mp3")
+elif "Senorita" in order:
+    playsound("C:\\Users\\Dell\\Downloads\\Senorita.mp3")
+elif "Bitches Come and go" in order:
+    playsound("C:\\Users\\Dell\\Downloads\\Bitches Come and go.mp3")
+elif "All My Friends are Toxic" in order:
+    playsound("C:\\Users\\Dell\\Downloads\\All My Friends are Toxic.mp3")
+   
+
+
+
